e820b8406e00d2cc7f8a4d585bba6d623702cffc
lukmak0394 <lukmak0394@wp.pl>
2022-06-12 17:08:45 +0200

    finished all basic functionalities

--- js/app.js
+++ js/app.js
@@ -1,41 +1,44 @@
-import { Github } from "./github.js";
-import { body, addClasses } from "./main.js";
+export {createContent}
 
 const searchBtn = document.querySelector('.search-btn');
-const username = document.querySelector('.username');
-const output = document.querySelector('.output-wrapper');
 const errorMsg = document.querySelector('.error-msg');
+const output = document.querySelector('.output-wrapper');
 
 const preloader = () => {
     output.innerHTML = '<img src="../assets/preloader.gif" alt="preloader" class="preloader">';
 }
 
+const createContent = (user) => {
+    output.innerHTML = `
+        <div class="basic-info-wrapper">
+            <img src="${user.avatar_url}" alt="user-avatar" class="avatar">
+            <div class="basic-info">
+                <h1 class="login">${user.name ? user.name : user.login}</h1>
+                <p class="created-at">Joined at ${new Date(user.created_at).toLocaleDateString()}</p>
+                <a href="${user.html_url}" target="_blank" class="user-link">@${user.login}</a>
+            </div>
+                <div class="bio">${user.bio ? user.bio : 'This profile has no bio'}</div>
+        </div>
+        <div class="stats">
+            <p class="stat repos">Repos<br><span class="stat-number">${user.public_repos}</span></p>
+            <p class="stat followers">Followers<br><span class="stat-number">${user.followers}</span></p>
+            <p class="stat following">Following<br><span class="stat-number">${user.following}</span></p>
+        </div> 
+    `;
+}
+
 const getData = () => {
 
-    const github = new Github;
-
-    github.getUsers(username.value)
+    fetch(`https://api.github.com/users/${username.value}`)
+    .then (res => res.json())
     .then (data => {
-        if (data.profile.message === "Not Found") {
+        
+        if (data.message === "Not Found") {
             errorMsg.classList.remove('no-show');
             output.innerHTML = '';
         } else {
             errorMsg.classList.add('no-show');
-            output.innerHTML = `
-                <div class="basic-info-wrapper">
-                    <img src="${data.profile.avatar_url}" alt="user-avatar" class="avatar">
-                    <div class="basic-info">
-                        <h1 class="login">${data.profile.login}</h1>
-                        <p class="created-at">Joined at ${new Date(data.profile.created_at).toLocaleDateString()}</p>
-                        <a href="${data.profile.html_url}" target="_blank" class="user-link">@${data.profile.login}</a>
-                    </div>
-                        <div class="bio">${data.profile.bio ? data.profile.bio : 'This profile has no bio'}</div>
-                </div>
-            `;
-
-            if (body.classList.contains('body-bg-dark')) {
-                addClasses();
-            }
+            createContent(data)
         }
     })
     .catch(error => {
@@ -43,11 +46,12 @@ const getData = () => {
         output.innerHTML = `<p>Couldn't load user data</p>`;
     })
 
+    
+
 }
 
 searchBtn.addEventListener('click',() => {
     if (username.value !== '') {
-        errorMsg.classList.add('no-show');
         preloader()
         setTimeout(getData,2000)
     }
--- js/main.js
+++ js/main.js
@@ -1,50 +1,34 @@
-export { body, addClasses }
+import { createContent } from "./app.js";
 
 const body = document.querySelector('body');
 const switchBtn = document.querySelector('.switch-mode-btn');
-const outputWrapper = document.querySelector('.output-wrapper');
-const logo = document.querySelector('.logo');
-const username = document.querySelector('.username');
 
 let mode = null;
-
 
 window.onload = () => {
 
     mode = localStorage.getItem('modeType');
 
     if (mode === 'dark') {
-        body.classList.add('body-bg-dark');
-        switchBtn.classList.add('switch-mode-btn-dark');
-        logo.classList.add('logo-dark');
-        username.classList.add('username-dark');
-        outputWrapper.classList.add('output-wrapper-dark');
+        body.classList.add('dark-mode');
         switchBtn.innerHTML = 'light <img src="assets/icon-sun.svg" alt="icon-sun" class="mode-icon">';
     } 
 
-  
-}
-
-const addClasses = () => {
-    document.querySelector('.login').classList.add('login-dark');
-    document.querySelector('.created-at').classList.add('created-at-dark');
-    document.querySelector('.bio').classList.add('bio-dark');
+    fetch(`https://api.github.com/users/octocat`)
+    .then (res => res.json())
+    .then (data => {
+        createContent(data)
+    })
+    .catch(error => {
+        console.log(error)
+        document.querySelector('.output-wrapper').innerHTML = `<p>Couldn't load user data</p>`;
+    })
 }
 
 const toggleClasses = () => {
 
-    body.classList.toggle('body-bg-dark') ;
+    body.classList.toggle('dark-mode') ;
     
-    switchBtn.classList.toggle('switch-mode-btn-dark');
-
-    logo.classList.toggle('logo-dark');
-    username.classList.toggle('username-dark');
-    outputWrapper.classList.toggle('output-wrapper-dark');
-
-    document.querySelector('.login') ? document.querySelector('.login').classList.toggle('login-dark') : false;
-    document.querySelector('.created-at') ? document.querySelector('.created-at').classList.toggle('created-at-dark') : false;
-    document.querySelector('.bio') ? document.querySelector('.bio').classList.toggle('bio-dark') : false;
-         
 }
 
 
@@ -52,7 +36,7 @@ const switchMode = () => {
 
     toggleClasses();
 
-    if (body.classList.contains('body-bg-dark')) {
+    if (body.classList.contains('dark-mode')) {
         switchBtn.innerHTML = 'light <img src="assets/icon-sun.svg" alt="icon-sun" class="mode-icon">';
         mode = localStorage.setItem('modeType','dark');
     } else {
