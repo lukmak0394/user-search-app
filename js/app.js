fd5504af48416e75af24112fcdb5028ec7a725fb
lukmak0394 <lukmak0394@wp.pl>
2022-06-12 17:14:04 +0200

    finished functions

--- js/app.js
+++ js/app.js
@@ -8,21 +8,21 @@ const preloader = () => {
     output.innerHTML = '<img src="../assets/preloader.gif" alt="preloader" class="preloader">';
 }
 
-const createContent = (user) => {
+const createContent = (userData) => {
     output.innerHTML = `
         <div class="basic-info-wrapper">
-            <img src="${user.avatar_url}" alt="user-avatar" class="avatar">
+            <img src="${userData.avatar_url}" alt="user-avatar" class="avatar">
             <div class="basic-info">
-                <h1 class="login">${user.name ? user.name : user.login}</h1>
-                <p class="created-at">Joined at ${new Date(user.created_at).toLocaleDateString()}</p>
-                <a href="${user.html_url}" target="_blank" class="user-link">@${user.login}</a>
+                <h1 class="login">${userData.name ? userData.name : userData.login}</h1>
+                <p class="created-at">Joined at ${new Date(userData.created_at).toLocaleDateString()}</p>
+                <a href="${userData.html_url}" target="_blank" class="user-link">@${userData.login}</a>
             </div>
-                <div class="bio">${user.bio ? user.bio : 'This profile has no bio'}</div>
+                <div class="bio">${userData.bio ? userData.bio : 'This profile has no bio'}</div>
         </div>
         <div class="stats">
-            <p class="stat repos">Repos<br><span class="stat-number">${user.public_repos}</span></p>
-            <p class="stat followers">Followers<br><span class="stat-number">${user.followers}</span></p>
-            <p class="stat following">Following<br><span class="stat-number">${user.following}</span></p>
+            <p class="stat repos">Repos<br><span class="stat-number">${userData.public_repos}</span></p>
+            <p class="stat followers">Followers<br><span class="stat-number">${userData.followers}</span></p>
+            <p class="stat following">Following<br><span class="stat-number">${userData.following}</span></p>
         </div> 
     `;
 }
